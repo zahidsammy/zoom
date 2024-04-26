61e93ce1aa2b88fdad5a3d0b39ac2042c49b7e6d
zahidsammy <zahidsammy95@gmail.com>
2024-04-27 09:51:08 +1200

    finished the sidebar

---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+export const SidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/Home.svg',
+
+    },
+    {
+        label: 'Upcoming',
+        route: '/Upcoming',
+        imgUrl: '/icons/Upcoming.svg',
+
+    },
+    {
+        label: 'previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg',
+
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/Video.svg',
+
+    },
+    {
+        label: 'Personal Room ',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg',
+
+    },
+]
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+'use client'
+import React, { use } from 'react'
+import { SidebarLinks } from '../../constants'
+import { usePathname } from 'next/navigation'
+import { Link } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 const Sidebar = () => {
+    const pathname = usePathname ();
   return (
-    <div>Sidebar</div>
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+        <div className='flex flex- flex-col gap-6'>
+
+            {SidebarLinks.map((link) => {
+                const isActive  = pathname === link.route || pathname.startsWith(link.route);
+
+                return(
+                    <Link href={link.route}
+                    key={link.label}
+                    className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{
+                        'bg-blue-1': isActive,
+                    })} >
+
+                    {link.label}
+                        
+                    </Link>
+                )
+            })}
+        </div>
+    </section>
   )
 }
 
